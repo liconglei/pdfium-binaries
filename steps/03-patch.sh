deb08a076d255de5fb37f23288d27025a04af3be
lcl <lcl@qmrjkj.com>
2026-05-27 16:58:08 +0800

    fix: use more flexible sed pattern for extern C closing

--- steps/03-patch.sh
+++ steps/03-patch.sh
@@ -107,8 +107,9 @@ FPDFAnnot_SetRefByObjNum(FPDF_ANNOTATION annot,
                          int genNum);
 EOF
 
-# Insert include right before the closing "} // extern C" line (inside the extern "C" block)
-sed -i '/^} \/\/ extern "C"/i #include "fpdf_annot_ext.h"' "$SOURCE/public/fpdf_annot.h"
+# Insert include right before the closing brace of extern "C" block
+# Match line containing "} // extern" which closes the block
+sed -i '/}.*extern.*C$/i #include "fpdf_annot_ext.h"' "$SOURCE/public/fpdf_annot.h"
 
 # Append implementation code to fpdf_annot.cpp
 cat "$PATCHES/annot-api/fpdf_annot_append.cpp" >> "$SOURCE/fpdfsdk/fpdf_annot.cpp"
